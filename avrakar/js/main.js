4c8f705a879af3a6fc49d593f94e7054cbf032a1
lyy9981 <lyy9981@gmail.com>
2014-05-14 08:30:43 -0400

    change off stage animation

--- avrakar/js/main.js
+++ avrakar/js/main.js
@@ -177,7 +177,7 @@ function handleClickEvent(evt) {
 	for (var i in displayObjects) { 
 		var displayObject = displayObjects[i];
 		var tweenMoveOut;
-		var yDestination = stage.canvas.height + displayObject.regY * 2; // regY = width/2
+		var yDestination = stage.canvas.height + 200; // 200 is the height of object
 		
 		// Animate objects off stage and call handleComplete.
 		if(i==0) {
