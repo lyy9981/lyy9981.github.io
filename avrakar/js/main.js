3fb48dd646eb3ef54d2dc99568461cb11fdd85fd
lyy9981 <lyy9981@gmail.com>
2014-05-14 08:37:10 -0400

    move methods around

--- avrakar/js/main.js
+++ avrakar/js/main.js
@@ -19,6 +19,98 @@ function init() {
 	stage.update();
 }
 
+function createDisplayObjects(choiceObjects) {
+	displayObjects.length = 0; // empty array
+	
+	for (var i in choiceObjects) {
+		var choiceObject = choiceObjects[i];
+		var sprite = new Image();
+	    sprite.src = choiceObject["src"];
+
+		var sheet = new createjs.SpriteSheet({
+		    images: [sprite], 
+		    frames: {width: choiceObject["width"], height: choiceObject["height"], regX: choiceObject["width"]/2, regY: choiceObject["height"]/2}, 
+		    animations: {	
+			    walk: [0, 9, "walk"]
+		    }
+	    });
+		
+		displayObject = new createjs.Sprite(sheet);	
+	    displayObject.name = choiceObject["name"];
+	    displayObject.x = choiceObject["x"];
+	    displayObject.y = choiceObject["y"];
+	    displayObject.scaleX = 0;
+	    displayObject.scaleY = 0;
+	
+		var tweenMoveIn = createjs.Tween.get(displayObject, {loop:false})
+		                         .to({scaleX:1.0, scaleY:1.0}, 1500, createjs.Ease.bounceOut);
+		
+	    stage.addChild(displayObject);
+	
+		displayObject.on("click", handleClickEvent);
+		displayObject.on("mouseover", handleMouseOverEvent);
+		displayObject.on("mouseout", handleMouseOutEvent);
+		
+		displayObjects.push(displayObject); // Add displayObject to array for later use
+	}
+}
+
+function handleMouseOverEvent(evt) {
+	selectedDisplayObject = evt.target;
+}
+
+function handleMouseOutEvent(evt) {
+	selectedDisplayObject = null;
+}
+
+function handleClickEvent(evt) {
+	var choiceValue = evt.target.name;
+	
+	createCookie(choiceType, choiceValue, 365);
+	output.text = ("Wah ha ha ha ! Tu as choisi " + readCookie(choiceType) + ".");
+	
+	for (var i in displayObjects) { 
+		var displayObject = displayObjects[i];
+		var tweenMoveOut;
+		
+		// Animate objects off stage and call handleComplete.
+		if(i==0) {
+			// We onlly want to call handleComplete once
+			tweenMoveOut = createjs.Tween.get(displayObject, {loop:false})
+			                         .to({scaleX:5.0, scaleY:5.0, alpha:0}, 1500, createjs.Ease.elasticIn).wait(10).call(handleComplete);
+		} else {
+			tweenMoveOut = createjs.Tween.get(displayObject, {loop:false})
+			                         .to({scaleX:5.0, scaleY:5.0, alpha:0}, 1500, createjs.Ease.elasticIn);
+		}
+
+	}
+}
+
+function handleComplete() {
+	if(choiceType == "gender") {
+		startStepTwo();
+	} else if(choiceType == "category") {
+		startStepThree();
+	}
+}
+
+function tick(event) {
+	for (var i in displayObjects) { 
+		var displayObject = displayObjects[i];
+		
+		// Pause the mouse over object, continue to animate the rest
+		if (selectedDisplayObject != displayObject) {
+			displayObject.play(); 
+		} else {
+			displayObject.stop(); 
+		}
+	}
+	
+	stage.update(event); 
+}
+
+// Steps
+
 function startStepOne() {
 	choiceType = "gender";
 	
@@ -124,95 +216,7 @@ function startStepThree() {
 	createDisplayObjects(raceChoices);
 }
 
-function createDisplayObjects(choiceObjects) {
-	displayObjects.length = 0; // empty array
-	
-	for (var i in choiceObjects) {
-		var choiceObject = choiceObjects[i];
-		var sprite = new Image();
-	    sprite.src = choiceObject["src"];
-
-		var sheet = new createjs.SpriteSheet({
-		    images: [sprite], 
-		    frames: {width: choiceObject["width"], height: choiceObject["height"], regX: choiceObject["width"]/2, regY: choiceObject["height"]/2}, 
-		    animations: {	
-			    walk: [0, 9, "walk"]
-		    }
-	    });
-		
-		displayObject = new createjs.Sprite(sheet);	
-	    displayObject.name = choiceObject["name"];
-	    displayObject.x = choiceObject["x"];
-	    displayObject.y = choiceObject["y"];
-	    displayObject.scaleX = 0;
-	    displayObject.scaleY = 0;
-	
-		var tweenMoveIn = createjs.Tween.get(displayObject, {loop:false})
-		                         .to({scaleX:1.0, scaleY:1.0}, 1500, createjs.Ease.bounceOut);
-		
-	    stage.addChild(displayObject);
-	
-		displayObject.on("click", handleClickEvent);
-		displayObject.on("mouseover", handleMouseOverEvent);
-		displayObject.on("mouseout", handleMouseOutEvent);
-		
-		displayObjects.push(displayObject); // Add displayObject to array for later use
-	}
-}
-
-function handleMouseOverEvent(evt) {
-	selectedDisplayObject = evt.target;
-}
-
-function handleMouseOutEvent(evt) {
-	selectedDisplayObject = null;
-}
-
-function handleClickEvent(evt) {
-	var choiceValue = evt.target.name;
-	
-	createCookie(choiceType, choiceValue, 365);
-	output.text = ("Wah ha ha ha ! Tu as choisi " + readCookie(choiceType) + ".");
-	
-	for (var i in displayObjects) { 
-		var displayObject = displayObjects[i];
-		var tweenMoveOut;
-		
-		// Animate objects off stage and call handleComplete.
-		if(i==0) {
-			// We onlly want to call handleComplete once
-			tweenMoveOut = createjs.Tween.get(displayObject, {loop:false})
-			                         .to({scaleX:5.0, scaleY:5.0, alpha:0}, 1500, createjs.Ease.elasticIn).wait(10).call(handleComplete);
-		} else {
-			tweenMoveOut = createjs.Tween.get(displayObject, {loop:false})
-			                         .to({scaleX:5.0, scaleY:5.0, alpha:0}, 1500, createjs.Ease.elasticIn);
-		}
-
-	}
-}
-
-function handleComplete() {
-	if(choiceType == "gender") {
-		startStepTwo();
-	} else if(choiceType == "category") {
-		startStepThree();
-	}
-}
-
-function tick(event) {
-	for (var i in displayObjects) { 
-		var displayObject = displayObjects[i];
-		
-		// Pause the mouse over object, continue to animate the rest
-		if (selectedDisplayObject != displayObject) {
-			displayObject.play(); 
-		} else {
-			displayObject.stop(); 
-		}
-	}
-	
-	stage.update(event); 
-}
+// Help methods
 
 function createCookie(name, value, days) {
     var expires;
